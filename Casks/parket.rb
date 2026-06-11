5afe290514a7439be6a1b65bf4a41665e2a1459e
timofey basuev <timofey@basuev.com>
2026-06-11 21:20:03 +0500

    chore: improve parket cask distribution

--- Casks/parket.rb
+++ Casks/parket.rb
@@ -1,11 +1,17 @@
 cask "parket" do
+  bundle_id = "com.parket.app"
+  codesign_requirement = '=designated => identifier "com.parket.app"'
+
   version "0.7.0"
   sha256 "e11742e2d1220279741a8f2bc0de31f7902502a86c46a9bb5c9962411abd684b"
 
   url "https://github.com/basuev/parket/releases/download/v#{version}/parket.zip"
   name "parket"
-  desc "minimal dwm-like tiling window manager for macOS"
+  desc "Minimal dwm-like tiling window manager"
   homepage "https://github.com/basuev/parket"
+
+  depends_on macos: :sonoma
+  depends_on arch: :arm64
 
   postflight do
     app_path = "/Applications/parket.app"
@@ -21,18 +27,19 @@ cask "parket" do
       system_command "/usr/bin/xattr",
                      args: ["-dr", "com.apple.quarantine", app_path]
       system_command "/usr/bin/codesign",
-                     args: ["--force", "--sign", "-", app_path]
+                     args: ["--force", "--sign", "-", "--requirements", codesign_requirement, app_path]
     else
       system_command "/bin/cp", args: ["-R", staged_app, app_path]
       system_command "/usr/bin/xattr",
                      args: ["-dr", "com.apple.quarantine", app_path]
       system_command "/usr/bin/codesign",
-                     args: ["--force", "--sign", "-", app_path]
+                     args: ["--force", "--sign", "-", "--requirements", codesign_requirement, app_path]
       system "open", "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility"
     end
   end
 
-  uninstall delete: "/Applications/parket.app"
+  uninstall quit:   bundle_id,
+            delete: "/Applications/parket.app"
 
-  zap trash: []
+  zap trash: "~/.config/parket"
 end
